c94e13489ded6e3a1ccfff9d7f58d0e875e72a42
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-10 21:38:33 -0300

    fix: some weird shit was happenin

--- src/math_utils/transform.cpp
+++ src/math_utils/transform.cpp
@@ -1,12 +1,10 @@
-#include <glm/ext/matrix_transform.hpp>
-#include <glm/glm.hpp>
-#include <glm/gtc/quaternion.hpp>
-
 #include "math_utils/transform.hpp"
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 using namespace phe::math;
 
-glm::mat4 toMatrix(const Transform &t) {
+glm::mat4 toMatrix(const Transform& t) {
     glm::mat4 translation = glm::translate(glm::mat4(1.0f), t.translation);
     glm::mat4 rotation    = glm::mat4_cast(t.rotation);
     glm::mat4 scale       = glm::scale(glm::mat4(1.0f), t.scale);
