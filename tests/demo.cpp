1e2a0fa66606b4c5116eedc1ff80c766b0f86c24
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-10 21:00:42 -0300

    add simple demo

--- tests/demo.cpp
+++ tests/demo.cpp
@@ -1,7 +1,27 @@
-#include <iostream>
+#include "graphics/window.hpp"
+
+#define WIDTH 800
+#define HEIGHT 600
+
+using namespace phe;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    auto window = graphics::createWindow(WIDTH, HEIGHT, "pheV3");
+    if (!window) {
+        return 1;
+    }
+
+    if (graphics::initWindow(window) < 0) {
+        graphics::destroyWindow(window);
+        return 1;
+    }
+
+    while (!graphics::shouldClose(window)) {
+        graphics::swapBuffers(window);
+        graphics::pollEvents();
+    }
+
+    graphics::destroyWindow(window);
     return 0;
 }
 
