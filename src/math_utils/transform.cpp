d439306a9c19a84d1c88c5a29c643b5e755e2c5f
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-11 17:46:17 -0300

    fix: add namespace name before function This was causing some issues before fixing. But it's all good now :)

--- src/math_utils/transform.cpp
+++ src/math_utils/transform.cpp
@@ -2,9 +2,7 @@
 #include <glm/ext/matrix_transform.hpp>
 #include <glm/gtc/quaternion.hpp>
 
-using namespace phe::math;
-
-glm::mat4 toMatrix(const Transform& t) {
+glm::mat4 phe::math::toMatrix(const Transform& t) {
     glm::mat4 translation = glm::translate(glm::mat4(1.0f), t.translation);
     glm::mat4 rotation    = glm::mat4_cast(t.rotation);
     glm::mat4 scale       = glm::scale(glm::mat4(1.0f), t.scale);
