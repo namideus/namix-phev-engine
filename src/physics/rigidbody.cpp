5e1ec642f4393cae45b8a92b69629e147881555a
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-12 19:34:47 -0300

    Refactor RigidBody implementation and add force integration Updated RigidBody constructor to initialize size, mass, and inertia tensor. Added calculateInertiaTensor function to compute inertia tensor based on mass and size. Implemented applyLinearForce and applyAngularForce for applying linear and angular forces, respectively. Added applyForce to simultaneously apply both linear and angular forces. Added integrateForces and integrateVelocity functions to update the velocity and position of the rigid body over time

--- src/physics/rigidbody.cpp
+++ src/physics/rigidbody.cpp
@@ -1,13 +1,78 @@
 #include "physics/rigidbody.hpp"
+#include <glm/ext/quaternion_geometric.hpp>
+#include <glm/ext/quaternion_trigonometric.hpp>
+#include <glm/geometric.hpp>
+#include <glm/matrix.hpp>
+
+#define LINEAR_DAMPING 0.9995
+#define ANGULAR_DAMPING 0.999 
+
+static glm::mat3 calculateInertiaTensor(float mass, glm::vec3 size) {
+    float w = size.x;
+    float h = size.y;
+    float d = size.z;
+
+    float ix = (1.0f / 12.0f) * mass * (h*h + d*d);
+    float iy = (1.0f / 12.0f) * mass * (w*w + d*d);
+    float iz = (1.0f / 12.0f) * mass * (w*w + h*h);
+
+    return glm::mat3(
+        ix, 0.0f, 0.0f,
+        0.0f, iy, 0.0f,
+        0.0f, 0.0f, iz
+    );
+}
 
 namespace phe::physics {
 
-RigidBody::RigidBody(float r, float g, float b) {
-    mesh = graphics::createCubeMesh(r, g, b);
+    RigidBody::RigidBody(glm::vec3 size, glm::vec3 rgb, float mass) 
+         : size(size), mass(mass) {
+
+    mesh = graphics::createCubeMesh(size, rgb);
+    inertiaTensor = calculateInertiaTensor(mass, size);
 }
 
 RigidBody::~RigidBody() {
     graphics::destroyMesh(mesh);
+}
+
+void applyLinearForce(RigidBody &rb, glm::vec3 f) {
+    rb.linearAcc += f / rb.mass; 
+}
+
+void applyAngularForce(RigidBody& rb, glm::vec3 f, glm::vec3 r) {
+    glm::vec3 t = glm::cross(r, f);
+    rb.angularAcc += glm::inverse(rb.inertiaTensor) * t; 
+}
+
+void applyForce(RigidBody& rb, glm::vec3 f, glm::vec3 r) {
+    applyLinearForce(rb, f);
+    applyAngularForce(rb, f, r);
+}
+
+void integrateForces(RigidBody& rb, float dt) {
+    // Integrate linear velocity.
+    rb.linearVel += rb.linearAcc * dt;
+    
+    // Integrate angular velocity
+    rb.angularVel += rb.angularAcc * dt;
+
+    // Reset linear and angular acceleration.
+    rb.linearAcc = glm::vec3(0.0f, 0.0f, 0.0f);
+    rb.angularAcc = glm::vec3(0.0f, 0.0f, 0.0f);
+}
+
+void integrateVelocity(RigidBody& rb, float dt) {
+    rb.trans.translation += rb.linearVel * dt;
+
+    glm::quat velQuat(0.0f, rb.angularVel.x, rb.angularVel.y, rb.angularVel.z);
+    glm::quat dq = 0.5f * rb.trans.rotation * velQuat * dt;
+
+    rb.trans.rotation += dq; 
+    rb.trans.rotation = glm::normalize(rb.trans.rotation);
+
+    rb.linearVel *= LINEAR_DAMPING;
+    rb.angularVel *= ANGULAR_DAMPING;
 }
 
 void setPosition(RigidBody& rb, float x, float y, float z) {
@@ -18,6 +83,17 @@ void setPosition(RigidBody& rb, float x, float y, float z) {
 
 void setPosition(RigidBody& rb, glm::vec3 newPosition) {
     rb.trans.translation = newPosition; 
+}
+
+void setRotation(RigidBody& rb, glm::quat q) {
+    rb.trans.rotation = q;
+}
+
+void setRotation(RigidBody &rb, float angleDegrees, glm::vec3 axis) {
+    // First normalize the axis;
+    axis = glm::normalize(axis);
+
+    rb.trans.rotation = glm::angleAxis(glm::radians(angleDegrees), axis);
 }
 
 } // namespace phe::physics
