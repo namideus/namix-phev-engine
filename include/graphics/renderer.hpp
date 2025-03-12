21a2d6614949f0ce4cb521b61215ea9d82c88423
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-12 13:10:40 -0300

    feat: implement renderInit and drawRigidBody functions updated renderInit to create the view and projection matrices then feed them to the shader program Enabled GL_DEPTH_TEST in renderInit for 3D visualization Added drawRigidBody function that draws the rigid body's mesh with the body's model matrix, extracted from its Transform component

--- include/graphics/renderer.hpp
+++ include/graphics/renderer.hpp
@@ -1,4 +1,8 @@
+#ifndef RENDERER_HPP
+#define RENDERER_HPP
+
 #include "graphics/shader.hpp"
+#include "physics/rigidbody.hpp"
 
 namespace phe::graphics {
     struct Renderer {
@@ -8,8 +12,16 @@ namespace phe::graphics {
         ~Renderer();
     };
 
-    Renderer renderInit();
+    Renderer renderInit(const float windowWidth, const float windowHeight);
 
     void renderClear(float r, float g, float b, float a);
+
+    /**
+     * Draws the rigid body in world space, feeding it's transform's model 
+     * matrix to the shader program.
+     */
+    void drawRigidBody(physics::RigidBody& rb, Renderer& r);
 }
+
+#endif
 
--- src/graphics/renderer.cpp
+++ src/graphics/renderer.cpp
@@ -1,4 +1,6 @@
 #include <GL/glew.h>
+#include <glm/ext/matrix_clip_space.hpp>
+#include <glm/ext/matrix_transform.hpp>
 
 #include "graphics/renderer.hpp"
 
@@ -6,16 +8,32 @@ namespace phe::graphics {
 
 void renderClear(float r, float g, float b, float a) {
     glClearColor(r, g, b, a);
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
-Renderer renderInit() {
+Renderer renderInit(const float windowWidth, const float windowHeight) {
     Renderer r;
     r.shader = new Shader("./src/graphics/shaders/vert.glsl", "./src/graphics/shaders/frag.glsl");
-
     r.shader->use();
 
+    glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+            glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f),
+            (float)windowWidth/(float)windowHeight, 0.1f, 1000.0f);
+
+    r.shader->setMat4("view", view);
+    r.shader->setMat4("projection", projection);
+
+    glEnable(GL_DEPTH_TEST);
+
     return r;
+}
+
+void drawRigidBody(physics::RigidBody& rb, Renderer& r) {
+    auto model = math::toMatrix(rb.trans);
+    r.shader->setMat4("model", model);
+    drawMesh(rb.mesh);
 }
 
 Renderer::Renderer() : shader(nullptr){ }
