1aa7c013c36476654059ba14e26fff27ea4f6f5f
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-15 19:25:05 -0300

    refactor collision utils: rename functions and add projection helper

--- include/physics/collisionUtils.hpp
+++ include/physics/collisionUtils.hpp
@@ -9,10 +9,12 @@ namespace phe::physics::collision {
  *
  * @return vector of face normals for a cube.
  */
-const std::vector<glm::vec3> computeCubeNormals(const RigidBody& rb);
-const std::vector<glm::vec3> computeCubeEdges(const RigidBody& rb);
+std::vector<glm::vec3> computeCubeNormals(const RigidBody& rb);
+std::vector<glm::vec3> computeCubeEdges(const RigidBody& rb);
 
-bool areOverlapping(const RigidBody& a, const RigidBody& b);
+bool areColliding(RigidBody& a, RigidBody& b);
+
+float projectOntoAxis(const glm::vec3& vertex, const glm::vec3& axis);
 
 } // namespace phe::physics::collision
 
