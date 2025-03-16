db4ee420dbee2ede3a755cfef63c1cf5aee17de2
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-15 23:51:27 -0300

    add collision detection demo

--- tests/collisionDetectionTest.cpp
+++ tests/collisionDetectionTest.cpp
@@ -9,6 +9,8 @@
 
 using namespace phe;
 
+const glm::vec3 GRAVITY(0.0f, -9.81, 0.0f);
+
 int main() {
     auto window = graphics::createWindow(800, 600, "Collision Detection Test");
     if (!window) {
@@ -24,13 +26,30 @@ int main() {
     physics::RigidBody rb1(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 0.0f, 0.0f), 1.0f);
     physics::RigidBody rb2(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f), 1.0f);
 
-    physics::setPosition(rb1, glm::vec3(-0.5f, 0.0f, 0.0f));
-    physics::setPosition(rb2, glm::vec3(0.78f, 0.0f, 0.0f));
+    physics::setPosition(rb1, glm::vec3(-2.0f, 1.0f, 0.0f));
+    physics::setPosition(rb2, glm::vec3(2.0f, 1.0f, 0.0f));
 
     physics::setRotation(rb2, 45.0f, glm::vec3(0.3f, 0.7f, 0.5f));
+    float lastTime = 0.0f;
+
+    physics::applyForce(rb1, glm::vec3(10.0f, 50.0f, -60.0f), glm::vec3(0.1, 0.1, 0.0f));
+    physics::applyForce(rb2, glm::vec3(-10.0f, 50.0f, -60.0f), glm::vec3(0.0, 0.1, 0.0));
 
     while (!graphics::shouldClose(window)) {
+        float currentTime = static_cast<float>(glfwGetTime());
+        float dt = currentTime - lastTime;
+        lastTime = currentTime;
+
         graphics::renderClear(0.2f, 0.3f, 0.3f, 1.0f);
+        
+        physics::applyForce(rb1, GRAVITY, glm::vec3(0.0f, 0.0f, 0.0f));
+        physics::applyForce(rb2, GRAVITY, glm::vec3(0.0f, 0.0f, 0.0f));
+
+        physics::integrateForces(rb1, dt);
+        physics::integrateForces(rb2, dt);
+
+        physics::integrateVelocity(rb1, dt);
+        physics::integrateVelocity(rb2, dt);
 
         graphics::drawRigidBody(rb1, renderer);
         graphics::drawRigidBody(rb2, renderer);
