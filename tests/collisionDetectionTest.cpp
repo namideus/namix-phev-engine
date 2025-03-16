13db82c8b2f37cf324c7f2f7cd5e4cee0f9a1554
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-16 11:25:19 -0300

    move collision detection and response to the top of the loop Change rigid bodies position and amount of force applied

--- tests/collisionDetectionTest.cpp
+++ tests/collisionDetectionTest.cpp
@@ -1,5 +1,4 @@
 // This was made to test collision detection between two Rigid Bodies.
-#include <iostream>
 #include <glm/glm.hpp>
 
 #include "graphics/renderer.hpp"
@@ -27,25 +26,26 @@ int main() {
     physics::RigidBody rb1(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(1.0f, 0.0f, 0.0f), 1.0f);
     physics::RigidBody rb2(glm::vec3(1.0f, 1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 1.0f), 1.0f);
 
-    physics::setPosition(rb1, glm::vec3(-2.0f, 1.0f, 0.0f));
-    physics::setPosition(rb2, glm::vec3(2.0f, 1.0f, 0.0f));
+    physics::setPosition(rb1, glm::vec3(-4.0f, 1.0f, 0.0f));
+    physics::setPosition(rb2, glm::vec3(4.0f, 1.0f, 0.0f));
 
     physics::setRotation(rb2, 45.0f, glm::vec3(0.3f, 0.7f, 0.5f));
     float lastTime = 0.0f;
 
-    physics::applyForce(rb1, glm::vec3(30.0f, 50.0f, -50.0f), glm::vec3(0.1, 0.1, 0.0f));
-    physics::applyForce(rb2, glm::vec3(-30.0f, 50.0f, -50.0f), glm::vec3(0.0, 0.1, 0.0));
+    physics::applyForce(rb1, glm::vec3(40.0f, 60.0f, -60.0f), glm::vec3(0.1, 0.1, 0.0f));
+    physics::applyForce(rb2, glm::vec3(-40.0f, 60.0f, -60.0f), glm::vec3(0.0, 0.1, 0.0));
 
     while (!graphics::shouldClose(window)) {
+        auto collisionInfo = physics::collision::areColliding(rb1, rb2);
+        if (collisionInfo.isColliding) {
+            physics::collision::resolveCollision(rb1, rb2, collisionInfo);
+        }
+
         float currentTime = static_cast<float>(glfwGetTime());
         float dt = currentTime - lastTime;
         lastTime = currentTime;
 
         graphics::renderClear(0.2f, 0.3f, 0.3f, 1.0f);
-        auto collisionInfo = physics::collision::areColliding(rb1, rb2);
-        if (collisionInfo.isColliding) {
-            physics::collision::resolveCollision(rb1, rb2, collisionInfo);
-        }
         
         physics::applyForce(rb1, GRAVITY, glm::vec3(0.0f, 0.0f, 0.0f));
         physics::applyForce(rb2, GRAVITY, glm::vec3(0.0f, 0.0f, 0.0f));
