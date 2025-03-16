a26e693960e7fe1e04e0e2c13695bea6e9420696
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-16 11:38:07 -0300

    feat: add PhysicsWorld class

---
+++ include/physics/physicsWorld.hpp
@@ -0,0 +1,30 @@
+#ifndef PHYSICS_WORLD_HPP
+#define PHYSICS_WORLD_HPP
+
+#include <vector>
+
+#include "physics/rigidbody.hpp"
+
+namespace phe::physics {
+
+/*
+ * PhysicsWorld class is ONLY responsible for applying GRAVITY, integrating
+ * forces to velocity, and integrating velocity to position of all rigid bodies
+ * it owns
+ */
+class PhysicsWorld {
+private:
+    std::vector<RigidBody> bodies;
+    const glm::vec3 GRAVITY = glm::vec3(0.0f, -9.80665, 0.0f);
+
+public:
+    void update(float dt);
+
+    void addRigidBody(RigidBody& rb);
+
+    ~PhysicsWorld();
+};
+
+}
+#endif
+
