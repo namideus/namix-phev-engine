9b2e65fc15a381222cff6e1f30ebe79d01c22b5c
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-15 23:48:29 -0300

    add CollisionInfo struct that holds collision info Refactor areColliding function to return an instance of CollisionInfo. This is needed to find the MPA, penetration depth and if the collision happened or not

--- include/physics/collisionUtils.hpp
+++ include/physics/collisionUtils.hpp
@@ -4,15 +4,21 @@
 
 namespace phe::physics::collision {
 
+struct CollisionInfo {
+    bool isColliding;
+    glm::vec3 mpa;          // Minimum Penetration Axis
+    float penetrationDepth; // How deep a rigid body is inside another
+};
+
 /**
- * The parameter rb NEEDS to have a cube mesh.
+ * The parameter rb NEEDS to have a CUBE mesh.
  *
  * @return vector of face normals for a cube.
  */
 std::vector<glm::vec3> computeCubeNormals(const RigidBody& rb);
 std::vector<glm::vec3> computeCubeEdges(const RigidBody& rb);
 
-bool areColliding(RigidBody& a, RigidBody& b);
+CollisionInfo areColliding(RigidBody& a, RigidBody& b);
 
 float projectOntoAxis(const glm::vec3& vertex, const glm::vec3& axis);
 
