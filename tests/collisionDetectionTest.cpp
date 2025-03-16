7e1b2e54f0b4abadb3e96e67b83b7e61697cf919
Felipe Dutra <felipemd.dutra@gmail.com>
2025-03-16 02:19:50 -0300

    super cool collision demo LETS GO COLLISIONS ARE WORKING WOOOOOOOOO

--- tests/collisionDetectionTest.cpp
+++ tests/collisionDetectionTest.cpp
@@ -33,8 +33,8 @@ int main() {
     physics::setRotation(rb2, 45.0f, glm::vec3(0.3f, 0.7f, 0.5f));
     float lastTime = 0.0f;
 
-    physics::applyForce(rb1, glm::vec3(90.0f, 50.0f, -60.0f), glm::vec3(0.1, 0.1, 0.0f));
-    physics::applyForce(rb2, glm::vec3(-90.0f, 50.0f, -60.0f), glm::vec3(0.0, 0.1, 0.0));
+    physics::applyForce(rb1, glm::vec3(30.0f, 50.0f, -50.0f), glm::vec3(0.1, 0.1, 0.0f));
+    physics::applyForce(rb2, glm::vec3(-30.0f, 50.0f, -50.0f), glm::vec3(0.0, 0.1, 0.0));
 
     while (!graphics::shouldClose(window)) {
         float currentTime = static_cast<float>(glfwGetTime());
@@ -43,6 +43,9 @@ int main() {
 
         graphics::renderClear(0.2f, 0.3f, 0.3f, 1.0f);
         auto collisionInfo = physics::collision::areColliding(rb1, rb2);
+        if (collisionInfo.isColliding) {
+            physics::collision::resolveCollision(rb1, rb2, collisionInfo);
+        }
         
         physics::applyForce(rb1, GRAVITY, glm::vec3(0.0f, 0.0f, 0.0f));
         physics::applyForce(rb2, GRAVITY, glm::vec3(0.0f, 0.0f, 0.0f));
@@ -55,10 +58,6 @@ int main() {
 
         graphics::drawRigidBody(rb1, renderer);
         graphics::drawRigidBody(rb2, renderer);
-
-        if (collisionInfo.isColliding) {
-            physics::collision::resolveCollision(rb1, rb2, collisionInfo);
-        }
 
         graphics::pollEvents();
         graphics::swapBuffers(window);
